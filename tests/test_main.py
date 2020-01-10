2d83750facbaa90f10897b8021bb513200cae7d3
littleround <lxy9843@gmail.com>
2020-01-10 18:12:58 +0800

    remove test related to examples

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,13 @@
 # First test case for the main function
 
+import os, pytest
 from pytest import raises
 from st2g.main import main
 
 
 def test_main_with_no_arg():
-    main([])
+    with raises(SystemExit):
+        main([])
 
 
 def test_main_asking_version():
@@ -19,6 +21,8 @@ def test_main_help():
 
 
 def test_main_process():
+    if not os.path.exists("examples/data/demo") or not os.path.exists("examples/data/TC"):
+        pytest.skip("skipping tests without TC", allow_module_level=True)
     main("run --input examples/data/demo/1.txt --output temp/collect/demo_1")
     main("run --input examples/data/demo/2.txt --output temp/collect/demo_2")
     main("run --input examples/data/TC/TC_C_1.txt --output temp/collect/tc_1")
