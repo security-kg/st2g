47870dbf5b2ba32e990ffd7b56add72972f152e5
littleround <lxy9843@gmail.com>
2020-01-10 18:04:05 +0800

    add output relation & entity

--- src/st2g/main.py
+++ src/st2g/main.py
@@ -1,5 +1,6 @@
 # entry point of the package
 import argparse
+import json
 from st2g.process import process_raw_text, output_result
 from st2g.representations import processContent, visualizeProcessedContent
 from st2g.relation_extraction import runRelationExtraction, convertEntitiesRelationsIntoDot
@@ -11,6 +12,8 @@ def main(unparsed_args=None):
     parser.add_argument("command", nargs="?", default="run")
     parser.add_argument("--input", "-i", type=str, required=True)
     parser.add_argument("--output", '-o', type=str, required=True)
+    parser.add_argument("--entity", '-E', action='store_true')
+    parser.add_argument("--relation", '-R', action='store_true')
     if unparsed_args:
         if isinstance(unparsed_args, str):
             unparsed_args = unparsed_args.split(" ")
@@ -31,6 +34,12 @@ def main(unparsed_args=None):
         dot.render(args.output+".dp")
         agg_result = sum(result, [])
         entities, relations = runRelationExtraction(agg_result)
+        if args.entity:
+            with open(args.output+".e.json", 'w') as fout:
+                json.dump(entities, fout)
+        if args.relation:
+            with open(args.output+".r.json", 'w') as fout:
+                json.dump(relations, fout)
         dot = convertEntitiesRelationsIntoDot(entities, relations)
         dot.render(args.output)
 
